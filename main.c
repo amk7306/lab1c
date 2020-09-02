70b515d7af7617c0b81635c74e21fbcdffb511ba
Abbey Kato <amk7306@psu.edu>
2020-09-02 11:52:29 +0000

    stuff

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
+  
+// Author: Abbey Kato amk7306@psu.edu
+// Collaboratorz: Jack Trueax jrt5617@psu.edu
+// Collaborator Jiahui Lan jzl6335@psu.edu
 #include <stdio.h>
 #include <readline/readline.h>
 #include <stdlib.h>
 
 int main(void) {
-char *ftemp = readline("Enter temperature in celsius: ");
-double temp = atof(ftemp);
-double ctemp = (temp/5)*9+32;
-printf("%.6f° in Celsius is equivalent to %.6f° Fahrenheit.",temp, ctemp );
-return 0;
+  char *ftemp = readline("Enter temperature in celsius: ");
+  double temp = atof(ftemp);
+  double ctemp = (temp/5)*9+32;
+  printf("%.6f° in Celsius is equivalent to %.6f° Fahrenheit.",temp, ctemp );
+  return 0;
 }
