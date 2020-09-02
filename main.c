b661094d7db1d2cefe74eea3731334d7885a026a
Abbey Kato <amk7306@psu.edu>
2020-09-02 02:26:39 +0000

    -

--- main.c
+++ main.c
@@ -7,5 +7,5 @@ char *ftemp = readline("Enter temperature in celsius: ");
 double temp = atof(ftemp);
 double ctemp = (temp/5)*9+32;
 printf("%.6f° in Celsius is equivalent to %.6f° Fahrenheit.",temp, ctemp );
-  return 0;
+return 0;
 }
